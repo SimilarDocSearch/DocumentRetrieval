211295ea8bc084d0e53273af072c2da38123fa88
Harshul Pandav <harshulpandav@gmail.com>
2015-04-19 17:24:32 -0400

    Added core classes and interfaces

---
+++ src/com/docrtrv/impl/BFSGenerator.java
@@ -0,0 +1,11 @@
+package com.docrtrv.impl;
+
+public class BFSGenerator {
+
+	//Main method just for testing purpose
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/docrtrv/master/Master.java
@@ -0,0 +1,10 @@
+package com.docrtrv.master;
+
+public interface Master {
+
+	public void getCosineSimilairty();
+	public void processSubRangeEstimation();
+	public void processOptDocRetrv();
+	public void processBFS();
+	
+}
